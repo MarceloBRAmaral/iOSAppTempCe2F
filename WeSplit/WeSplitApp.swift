82c2064e6dc000277a0780d16520784f18c84115
MarceloBRAmaral <marceloamaral.amaral@gmail.com>
2022-01-27 19:39:58 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Marcelo on 2022-01-27.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
